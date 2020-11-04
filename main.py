a530e68b200fd4793a564d05d6baee80c1c85484
Albert Baiges <albertbaigesjabega@gmail.com>
2020-11-04 21:05:19 +0100

    Tweepy ready to use

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import tweepy
+from tweepy.parsers import JSONParser
 
-consumer_key= 'yourkeyhere'
-consumer_secret= 'yourkeyhere'
-access_token= 'yourkeyhere'
-access_token_secret= 'yourkeyhere'
+import json
 
 
+consumer_key= 'CKXyrLIRjOoEPwDisGM3uLvSN'
+consumer_secret= '4F9Xg0kawfekbMkt96Tjo97o7RzTz2LV9UKxYiuc5nVFOOOT9K'
+access_token= '988767977796403201-vqhmrMjU30V2FceVUGC6AyQeox1FAOD'
+access_token_secret= 'bCr1GYdXEofxbI09ix96Y943uLsnop7wRftAMgi044lfd'
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, parser = JSONParser())
+
+keyword = "#TrumpvsBiden"
+numTweets = 1
+listaTweets = api.search(keyword, count = numTweets)
+
+
+print(listaTweets["statuses"][0]["user"]["screen_name"]) 
