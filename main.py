ddd3ffa0a5925706fce52c02c07b63d857cb4d20
Albert Baiges <albertbaigesjabega@gmail.com>
2020-11-05 02:27:30 +0100

    Scrapping countries and dataframe creation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tweepy
 from tweepy.parsers import JSONParser
-
-import json
+import requests
+import re
+from lxml import html
+import pandas as pd
 
 
 consumer_key= 'CKXyrLIRjOoEPwDisGM3uLvSN'
@@ -14,9 +16,22 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, parser = JSONParser())
 
-keyword = "#TrumpvsBiden"
-numTweets = 1
-listaTweets = api.search(keyword, count = numTweets)
+keyword = "#TrumpvsBiden" 
+numTweets = 2
+tweets = api.search(keyword, count = numTweets)
 
+#for tweet in tweets["statuses"]:
+#    print(tweet)
 
-print(listaTweets["statuses"][0]["user"]["screen_name"]) 
+#GET THE LIST OF COUNTRIES, SCRAPPING WIKIPEDIA
+response = requests.get("https://en.wikipedia.org/wiki/List_of_sovereign_states")
+tree = html.fromstring(response.text)
+table = tree.xpath("//table[1]/tbody/tr/td[1]/text()")
+regex = re.compile('^([ ]+)|.[^a-zA-Z ].|([ ]+)$|[^a-zA-Z ]'); # Try to reduce complexity of this regex, but maintaining functionality
+countries = list(filter(lambda x: x!="" and x!= " Other states " and x!="and", list(map(lambda x: regex.sub("", x), table))))
+#print(countries)
+
+# Setting up the dataframe
+df = pd.DataFrame(index = countries, columns = ["Counter"])
+df.loc[:, :] = 0
+print(df)
