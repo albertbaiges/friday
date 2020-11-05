3cdc0201d72d2633d1e2ba6cb98256856fa81c81
Albert Baiges <albertbaigesjabega@gmail.com>
2020-11-05 02:37:49 +0100

    Updated requirements.txt and reordered main.py

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from lxml import html
 import pandas as pd
 
 
+#GET THE LIST OF COUNTRIES, SCRAPPING WIKIPEDIA
+response = requests.get("https://en.wikipedia.org/wiki/List_of_sovereign_states")
+tree = html.fromstring(response.text)
+table = tree.xpath("//table[1]/tbody/tr/td[1]/text()")
+regex = re.compile('^([ ]+)|.[^a-zA-Z ].|([ ]+)$|[^a-zA-Z ]'); # Try to reduce complexity of this regex, but maintaining functionality
+countries = list(filter(lambda x: x!="" and x!= " Other states " and x!="and", list(map(lambda x: regex.sub("", x), table))))
+#print(countries)
+
+# SETTING UP THE DATAFRAME
+df = pd.DataFrame(index = countries, columns = ["Counter"])
+df.loc[:, :] = 0
+#print(df)
+
+# TWITTER DATA
 consumer_key= 'CKXyrLIRjOoEPwDisGM3uLvSN'
 consumer_secret= '4F9Xg0kawfekbMkt96Tjo97o7RzTz2LV9UKxYiuc5nVFOOOT9K'
 access_token= '988767977796403201-vqhmrMjU30V2FceVUGC6AyQeox1FAOD'
@@ -22,16 +36,3 @@ tweets = api.search(keyword, count = numTweets)
 
 #for tweet in tweets["statuses"]:
 #    print(tweet)
-
-#GET THE LIST OF COUNTRIES, SCRAPPING WIKIPEDIA
-response = requests.get("https://en.wikipedia.org/wiki/List_of_sovereign_states")
-tree = html.fromstring(response.text)
-table = tree.xpath("//table[1]/tbody/tr/td[1]/text()")
-regex = re.compile('^([ ]+)|.[^a-zA-Z ].|([ ]+)$|[^a-zA-Z ]'); # Try to reduce complexity of this regex, but maintaining functionality
-countries = list(filter(lambda x: x!="" and x!= " Other states " and x!="and", list(map(lambda x: regex.sub("", x), table))))
-#print(countries)
-
-# Setting up the dataframe
-df = pd.DataFrame(index = countries, columns = ["Counter"])
-df.loc[:, :] = 0
-print(df)
