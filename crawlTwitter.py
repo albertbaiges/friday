2ceaba44c469477a3265f7627ae784e65163cfb2
Albert Baiges <69116384+albertbaiges@users.noreply.github.com>
2021-02-19 15:41:13 +0100

    Update crawlTwitter.py

--- crawlTwitter.py
+++ crawlTwitter.py
@@ -6,10 +6,10 @@ from tweepy.parsers import JSONParser
 
 
 def tweetsWithKeywordJSON(keyword, samples):
-    consumer_key= 'CKXyrLIRjOoEPwDisGM3uLvSN'
-    consumer_secret= '4F9Xg0kawfekbMkt96Tjo97o7RzTz2LV9UKxYiuc5nVFOOOT9K'
-    access_token= '988767977796403201-vqhmrMjU30V2FceVUGC6AyQeox1FAOD'
-    access_token_secret= 'bCr1GYdXEofxbI09ix96Y943uLsnop7wRftAMgi044lfd'
+    consumer_key= ''
+    consumer_secret= ''
+    access_token= ''
+    access_token_secret= ''
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
